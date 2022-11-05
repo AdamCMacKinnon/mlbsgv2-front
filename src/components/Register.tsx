f87f031a99c52c1892881694b9d15ae1c3ce6c32
Christopher Owens <cowensdev@gmail.com>
2022-11-05 17:22:19 -0400

    Updated styles/add logic to handle server connection for sign up

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import RegForm from './forms/regForm'
 
+import styled from 'styled-components';
+
+const RegistrationForm = styled.div`
+  margin: 200px auto;
+  width: 80%;
+  max-width: 500px;
+  padding: 20px;
+  background-color: lightcyan;
+  color:rgb(6, 128, 55);
+  border-radius: 10%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
 function Register() {
   return (
-    <div className="regForm"><h1>REGISTER</h1>
+    <RegistrationForm><h1>REGISTER</h1>
         <RegForm /><br></br>
     <span><a href="/">Back to Home</a></span><br></br>
-    </div>
+    </RegistrationForm>
   )
 }
 
--- src/components/forms/regForm.tsx
+++ src/components/forms/regForm.tsx
@@ -1,38 +1,90 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
+import { useState } from "react";
+import styled from "styled-components"
+import axios from "axios";
+
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 
+
+
 export default function FormPropsTextFields() {
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const currentDate = new Date();
+    const isoDate = currentDate.toISOString();
+
+    try{
+      const response = await axios.post(`${process.env.REACT_APP_SERVER}/auth/register`, {
+        "username": userName,
+        "password": password,
+        "email": email.toLowerCase(),
+        "isactive": true,
+        "admin": false,
+        "pastchamp": false,
+        "diff": 0,
+        "createdAt": isoDate,
+        "updatedAt": isoDate        
+      })
+      if (response.status === 201) {
+        setUserName('')
+        setEmail('')
+        setPassword('')
+        alert('User successfully created');
+      }
+    } catch (e : any) {
+      console.log(e.response.data.message)
+      setErrorMessage(e.response.data.message)
+    }
+  }
+
+  
   return (
-    <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-      }}
+    <FormBox
       noValidate
       autoComplete="off"
     >
-      <div>
+      <ErrorMessage>{errorMessage && errorMessage}</ErrorMessage>
         <TextField
           required
           id="outlined-required"
-          label="Required"
-          defaultValue="Username"
+          label="User Name"
+          value={userName}
+          onChange={e => setUserName(e.target.value)}
         />
         <TextField
-        id="outline"
-        label="email"
-        defaultValue="email"
+          id="outline"
+          label="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
         <TextField
           id="outlined-password-input"
           label="Password"
           type="password"
           autoComplete="current-password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
         />
-      </div>
-      <Button variant="contained">Submit</Button>
-    </Box>
+      <Button variant="contained" onClick={e => handleSubmit(e)}>Submit</Button>
+    </FormBox>
   );
 }
+
+const FormBox = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    max-width: 300px;
+    gap: 10px;   
+`
+
+const ErrorMessage = styled.div`
+  color: red;
+  margin: 0 auto;
+`
