30cc2b76d0b7547ef13b1768b476493f6db45050
Christopher Owens <cowensdev@gmail.com>
2022-11-06 09:55:09 -0500

    Updated styles/add logic to handle server connection for sign up

--- src/components/forms/loginForm.tsx
+++ src/components/forms/loginForm.tsx
@@ -1,33 +1,79 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 
-export default function FormPropsTextFields() {
+import styled from 'styled-components';
+import axios from "axios";
+
+export default function LoginForm(props: any) {
+const [username, setUsername] = useState('')
+const [password, setPassword] = useState('')
+const [errorMessage, setErrorMessage] = useState('');
+const navigate = useNavigate();
+
+// const { setToken } = props;
+
+const handleSubmit = async () => {  
+  try {
+    const response = await axios.post(`${process.env.REACT_APP_SERVER}/auth/login`, {
+      username,
+      password
+    })
+    if (response.status === 201) {
+      setUsername('')
+      setPassword('')
+      setErrorMessage('')
+      // setToken(response.data)
+      navigate('/')
+    }
+  } catch (e) {
+    console.log(e)
+    console.log(username)
+    console.log(password)
+    if(e) {
+      setErrorMessage('Invalid login');
+    }
+  }
+}
   return (
-    <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-      }}
+    <Form
       noValidate
       autoComplete="off"
     >
-      <div>
+      
         <TextField
           required
           id="outlined-required"
-          label="Required"
-          defaultValue="Username"
+          label="Username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
         />
         <TextField
           id="outlined-password-input"
           label="Password"
           type="password"
           autoComplete="current-password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
         />
-      </div>
-      <Button variant="contained">Submit</Button>
-    </Box>
+      
+      <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+      <ErrorMessage>{errorMessage ? errorMessage : ' '}</ErrorMessage>
+    </Form>
   );
 }
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    max-width: 300px;
+    gap: 10px;   
+`
+
+const ErrorMessage = styled.p`
+  color: red;
+  margin: 0 auto;
+  height: 10px;
+`
