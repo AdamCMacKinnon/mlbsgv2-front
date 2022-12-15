b7df6c3ef4ddb02605a3a27961220c4e96b90533
Christopher Owens <cowensdev@gmail.com>
2022-12-15 10:17:31 -0500

    Fix styling issues for desktop/mobile

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 import RegForm from './forms/regForm'
 
 import styled from 'styled-components';
+
+
+
+export default function Register() {
+  return (
+    <RegistrationContainer>
+      <RegistrationForm>
+        <h1>REGISTER</h1>
+        <RegForm />
+        <Link to="/">Back to Home</Link>
+      </RegistrationForm>
+    </RegistrationContainer>
+
+  )
+}
+
+
+
+const RegistrationContainer = styled.div`
+  height: 97vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`
 
 const RegistrationForm = styled.div`
   margin: 200px auto;
@@ -10,19 +36,12 @@ const RegistrationForm = styled.div`
   padding: 20px;
   background-color: lightcyan;
   color:rgb(6, 128, 55);
-  border-radius: 10%;
+  border-radius: 30px;
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  a{
+    margin: 20px;
+  }
 `
-
-function Register() {
-  return (
-    <RegistrationForm><h1>REGISTER</h1>
-        <RegForm /><br></br>
-    <span><a href="/">Back to Home</a></span><br></br>
-    </RegistrationForm>
-  )
-}
-
-export default Register
