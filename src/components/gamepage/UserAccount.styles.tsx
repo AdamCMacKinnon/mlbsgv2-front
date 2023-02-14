4ef68b81491fd9180faa513695fe9ec6ef6e1d7a
Christopher Owens <cowensdev@gmail.com>
2023-02-13 21:02:00 -0500

    Add user Account update page

---
+++ src/components/gamepage/UserAccount.styles.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const UserAccountContainer = styled.div`
+  background-color: white;
+  width: 80%;  
+  max-width: 600px;
+  padding: 20px;
+  color:rgb(6, 128, 55);
+  border-radius: 30px;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-self: center;
+  margin: 20px auto 0;
+  text-align: center;
+`
