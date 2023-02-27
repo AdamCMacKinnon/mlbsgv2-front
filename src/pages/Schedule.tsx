524234bf08e758976f56c0b0cfcd67779d98e572
Christopher Owens <cowensdev@gmail.com>
2023-02-26 20:29:36 -0500

    Issue 14: Added link to top of schedule page

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -8,7 +8,7 @@ import SelectTeamFilter from '../components/admin/SelectTeamFilter';
 import {displaySchedule, getDateTimeByDifference} from '../functions/index';
 
 //Styles
-import { ScheduleContainer, FilterContainer } from './Schedule.styles';
+import { ScheduleContainer, FilterContainer, GameListContainer, GamePageLink } from './Schedule.styles';
 import ScheduleGameCard from '../components/schedule/ScheduleGameCard';
 
 
@@ -16,25 +16,27 @@ import ScheduleGameCard from '../components/schedule/ScheduleGameCard';
 const Schedule = () => {
   const [dateFrom, setDateFrom] = useState(getDateTimeByDifference(0));
   const [dateTo, setDateTo] = useState("2023-04-06");
-  const [team, setTeam] = useState('New York Yankees');
+  const [team, setTeam] = useState('None');
   const schedule = displaySchedule(dateFrom, dateTo, team);
 
   const displayScheduleGames = schedule.length > 0 ? schedule.map((game: any) => <ScheduleGameCard team={team} game={game} key={game.gamePk}/>) : (<p style={{color: 'white'}}>No games for this time period</p>);
   return (
     <ScheduleContainer>
+      <GamePageLink to="/gamepage">Go To Gamepage</GamePageLink>
       <FilterContainer>
-      <DatePicker label="Date From" date={dateFrom} setDate={setDateFrom}/>
-      <DatePicker label="Date To" date={dateTo} setDate={setDateTo}/>
-      <SelectTeamFilter filterPickValue={team} setFilterPickValue={setTeam}/>
-      <div className="homeaway">
-        <div className="home"></div>
-        Home 
-        <div className="away"></div>
-        Away
-      </div>
+        <DatePicker label="Date From" date={dateFrom} setDate={setDateFrom}/>
+        <DatePicker label="Date To" date={dateTo} setDate={setDateTo}/>
+        <SelectTeamFilter filterPickValue={team} setFilterPickValue={setTeam}/>
+        <div className="homeaway">
+          <div className="home"></div>
+          Home 
+          <div className="away"></div>
+          Away
+        </div>
       </FilterContainer>
-      {displayScheduleGames}    
-      
+      <GameListContainer>
+        {displayScheduleGames} 
+      </GameListContainer>
     </ScheduleContainer>
   )
 }
