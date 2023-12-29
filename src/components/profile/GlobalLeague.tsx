c54c86818ca22b03f6bd0334207e3c69ff9ede93
Adam C. MacKinnon <adamcmackdev@gmail.com>
2023-12-29 11:16:56 -0500

    formatting because im antsy

--- src/components/profile/GlobalLeague.tsx
+++ src/components/profile/GlobalLeague.tsx
@@ -6,30 +6,35 @@ import { enterGlobalLeague } from "../../functions";
 import DisplayMessage from "../DisplayMessage";
 
 export default function GlobalLeague(props: any) {
-    const [apiResponse, setApiResponse] =  useState({});
-    const [open, setOpen] = useState(false);
-  
+  const [apiResponse, setApiResponse] = useState({});
+  const [open, setOpen] = useState(false);
 
-    const { user, token } = props;
-    const handleGlobalEntry = async (e: any) => {
-
-        const response: any = await enterGlobalLeague(token);
-        setApiResponse(response);
-        if (!response) {
-            setOpen(false);
-        } else {
-            setOpen(true)
-        }
-    }  
+  const { token } = props;
+  const handleGlobalEntry = async (e: any) => {
+    const response: any = await enterGlobalLeague(token);
+    setApiResponse(response);
+    if (!response) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  };
   return (
     <>
-            <Button onClick={handleGlobalEntry} variant="contained" color="error" sx={{padding: '10px', marginBottom: '10px'}}>JOIN THE GLOBAL LEAGUE</Button>
-            <br />
+      <Button
+        onClick={handleGlobalEntry}
+        variant="contained"
+        color="error"
+        sx={{ padding: "10px", marginBottom: "10px" }}
+      >
+        JOIN THE GLOBAL LEAGUE
+      </Button>
+      <br />
       <DisplayMessage response={apiResponse} open={open} setOpen={setOpen} />
       <Header>
         <h3>Current Global League Standings:</h3>
       </Header>
-      <LeaderBoard /> 
+      <LeaderBoard />
     </>
   );
 }
